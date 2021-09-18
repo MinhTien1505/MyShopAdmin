0b17aa66cd3de46a1b616d50da27a52338d58537
tiensony1505 <tiensony1505@gmail.com>
2021-09-18 21:10:47 +0700

    fix slidebar

--- src/router/index.js
+++ src/router/index.js
@@ -1,35 +1,39 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Login from '../pages/Admin/Login.vue'
-import Dashboard from '../pages/Admin/Dashboard.vue'
-import Products from '../pages/Admin/Products.vue'
-import CreateProduct from '../pages/Admin/CreateProduct.vue'
-import EditProduct from '../pages/Admin/EditProduct.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Login from "../pages/admin/Login.vue";
+import Dashboard from "../pages/admin/Dashboard.vue";
+import Products from "../pages/admin/Products.vue";
+import CreateProduct from "../pages/admin/CreateProduct.vue";
+import EditProduct from "../pages/admin/EditProduct.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Login',
-    component: Login
+    path: "/",
+    name: "Login",
+    component: Login,
   },
   {
-    path: '/dashboard',
-    name: 'Dashboard',
+    path: "/dashboard",
+    name: "Dashboard",
     component: Dashboard,
     children: [
-      { path: 'products', name: "ListProduct", component: Products },
-      { path: 'create-product', name: "CreateProduct", component: CreateProduct },
-      { path: 'edit-product', name: "EditProduct", component: EditProduct },
-    ]
-  }
-]
+      { path: "products", name: "ListProduct", component: Products },
+      {
+        path: "create-product",
+        name: "CreateProduct",
+        component: CreateProduct,
+      },
+      { path: "edit-product", name: "EditProduct", component: EditProduct },
+    ],
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
