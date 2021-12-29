f037c687297c26386536b6ec912f79e5e42ed6e7
MinhTien <tiensony1505@gmail.com>
2021-12-29 20:14:25 +0700

    deploy

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,6 @@ module.exports = {
   devServer: {
     proxy: "https://shopfreshapi.herokuapp.com",
     //proxy: "http://localhost:5000",
+    publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
   },
 };
